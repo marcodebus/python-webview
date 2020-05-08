32fa0a5572d6ce1a23e176bb4e0b68b7a37397cd
Marco Debus <33735167+marcodebus@users.noreply.github.com>
2020-05-08 15:16:43 +0200

    Create new.py

---
+++ new.py
@@ -0,0 +1,28 @@
+import webview
+import sys
+
+pwd_file = 'pwd.txt'
+
+
+class Api:
+    password = '12345'
+    app = None
+
+    def __init__(self, password):
+        self.password = password
+
+    def setApp(self, app):
+        self.app = app
+
+
+def read_pwd(input_file):
+    with open(input_file) as opened_file:
+        return opened_file.read().strip()
+
+
+if __name__ == '__main__':
+    pwd = read_pwd(pwd_file)
+    api = Api(pwd)
+    view = webview.create_window('Full-screen window', 'view.html', fullscreen=True, js_api=api)
+    api.setApp(view)
+    webview.start()
